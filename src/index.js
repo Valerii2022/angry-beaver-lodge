64fd461ae4954cfa26fb0fc85bbeadd0110f1fa4
Valerii Pometun <112505741+Valerii2022@users.noreply.github.com>
2024-08-14 15:39:57 +0300

    update for vercel deploy

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/angry-beaver-lodge">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
