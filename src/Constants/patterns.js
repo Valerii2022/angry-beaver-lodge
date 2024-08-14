19afe245016a5ad4a144df3cae20fb37c8899e74
Valerii Pometun <112505741+Valerii2022@users.noreply.github.com>
2024-08-14 14:44:44 +0300

    update pattern

--- src/Constants/patterns.js
+++ src/Constants/patterns.js
@@ -1,3 +1,3 @@
-export const namePattern = /^[A-Za-z]+$/;
+export const namePattern = /^[A-Za-z' -]*$/;
 export const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 export const phonePattern = /^\d{3}-\d{3}-\d{4}$/;
